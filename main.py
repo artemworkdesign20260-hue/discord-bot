122e2e2c6bdaca8b5901cf98ead48a29d4e3b726
artemworkdesign20260-hue <artem.work.design20260@gmail.com>
2026-08-22 13:40:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ async def on_message(message):
 
                 # Запит до Gemini
                 response = client.models.generate_content(
-                    model="gemini-2.5-flash",
+                    model="gemini-2.0-flash",
                     contents=user_text,
                 )
 
