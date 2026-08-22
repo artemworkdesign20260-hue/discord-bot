bf9460e52104355f30378a316fb85728a65ad524
artemworkdesign20260-hue <artem.work.design20260@gmail.com>
2026-08-22 10:00:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ async def on_message(message):
                 )
 
                 response = client.models.generate_content(
-                    model='gemini-2.5-flash',
+                    model="gemini-1.5-flash",
                     contents=prompt,
                 )
                 await message.channel.send(response.text)
